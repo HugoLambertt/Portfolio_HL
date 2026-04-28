0a57c29227366f301bc3b628d8195ec839d898a7
Hugo Lambert <160122575+HugoLambertt@users.noreply.github.com>
2026-04-28 10:20:05 +0200

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -59,7 +59,7 @@ const Contact = () => {
       icon: Shield,
       href: 'https://profile.hackthebox.com/profile/019c86c8-7c5c-7307-8cbc-55bfc2836639',
       color: 'accent',
-      label: 'Hack The Box (Classement en 60ème position parmis les campus Ynov)',
+      label: 'Classé Top 53 / Ynov Campus sur la plateforme Hack The Box',
     },
   ];
 
